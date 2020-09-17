b222b71520c21bc5e0125a0687d137c2ab4aee15
A <alisonchen@a.local>
2020-09-17 15:33:10 -0400

    COmmit 3

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,13 +1,21 @@
 import React from 'react';
  
+import { Link } from 'react-router-dom';
 
-
-function ArticleCard({articleData}) {
-    console.log('articleData', articleData);
-
+function ArticleCard({ articleData }) {
     return(
-        <div>
-            <h2>{articleData.title}</h2>
+        <div className="ArticleCard">
+            <div className="ArticleCardImage">
+                <img src={articleData.image.url} alt={articleData.image.alt} /> 
+            </div>
+            <div className="ArticleCardText">
+                <h2>{articleData.title}</h2>
+                <p> {articleData.publishedDate}</p>
+                <p> {articleData.blurb}</p>
+                <Link to={`article/${articleData.id}`}> Read More</Link>
+            </div>
+
+
         </div>
     );
 }
